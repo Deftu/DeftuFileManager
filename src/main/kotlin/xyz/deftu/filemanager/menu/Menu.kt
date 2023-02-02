6c6650216c4a85545b721275c6f9a2aa155e64dd
Maximusbarcz <maxim.baranek@gmail.com>
2023-02-02 21:37:26 +0100

    Made the menu mildly work, its usable, needs some fixing but at least works! added a bunch of comments because its hard to orient in java swing

--- src/main/kotlin/xyz/deftu/filemanager/menu/Menu.kt
+++ src/main/kotlin/xyz/deftu/filemanager/menu/Menu.kt
@@ -1,4 +1,3 @@
-package xyz.deftu.filemanager.menu
 
 import java.awt.*
 import java.awt.event.ActionEvent
@@ -6,15 +5,7 @@ import java.awt.event.ActionListener
 import java.awt.event.WindowEvent
 import java.awt.event.WindowListener
 import java.awt.geom.RoundRectangle2D
-import java.net.URI
-import javax.swing.BorderFactory
-import javax.swing.BoxLayout
-import javax.swing.JButton
-import javax.swing.JFrame
-import javax.swing.JLabel
-import javax.swing.JPanel
-import javax.swing.JScrollPane
-import javax.swing.UIManager
+import javax.swing.*
 
 class Menu : JFrame(
     "File Manager"
@@ -30,18 +21,24 @@ class Menu : JFrame(
 
     private val resourceLoader = MenuResourceLoader()
 
+    //the main 3 rows
     private val titleRow = JPanel()
     private val outputRow = JPanel()
     private val actionRow = JPanel()
 
+    //the menu title
     private val titleLabel = JLabel("File Manager")
+    //the output area scrollbar, text area, and the list with the items
     private val outputArea = JScrollPane()
-    private val outputText = JLabel()
+    private val outputText = JTextPane()
     private val outputList = mutableListOf<String>()
+    //buttons!
     private val supportButton = JButton("Support")
     private val closeButton = JButton("Close")
 
     private var support: String? = null
+
+    // ???
     var shouldExit = false
 
     fun display(
@@ -55,32 +52,37 @@ class Menu : JFrame(
 
         UIManager.put("Button.select", background2)
 
+        //title label, should be fine
         titleLabel.foreground = textColor
         titleLabel.font = resourceLoader.header2Font
         titleLabel.horizontalAlignment = JLabel.CENTER
 
         outputText.foreground = textColor
+        outputText.background = background3
+        outputText.isEditable = false
+        outputText.contentType = "text/html"
         outputText.font = resourceLoader.bodyFont
-        outputText.horizontalAlignment = JLabel.CENTER
-
-        val outputAreaPane = JPanel()
 
         outputArea.background = background3
         outputArea.isOpaque = false
         outputArea.border = BorderFactory.createEmptyBorder()
         outputArea.viewport.isOpaque = false
         outputArea.viewport.add(outputText)
+        outputArea.viewportBorder = BorderFactory.createEmptyBorder(0, 15, 0, 15)
+        outputArea.verticalScrollBar.autoscrolls = true
 
         supportButton.isFocusPainted = false
         supportButton.font = resourceLoader.buttonFont
         supportButton.foreground = textColor
         supportButton.background = background3
         supportButton.addActionListener(this)
+        //TODO: add this only on hover
         supportButton.border = BorderFactory.createCompoundBorder(
             BorderFactory.createLineBorder(accentColor, 2, true),
             BorderFactory.createEmptyBorder(5, 10, 5, 10)
         )
 
+        //TODO: make this not goofy
         val space = JLabel(" ")
         space.foreground = textColor
         space.font = resourceLoader.bodyFont
@@ -91,38 +93,32 @@ class Menu : JFrame(
         closeButton.foreground = textColor
         closeButton.background = background3
         closeButton.addActionListener(this)
+        //TODO: add this only on hover
         closeButton.border = BorderFactory.createCompoundBorder(
             BorderFactory.createLineBorder(accentColor, 2, true),
             BorderFactory.createEmptyBorder(5, 10, 5, 10)
         )
 
-        // Setup rows
+        // Setup rows | Works!
         titleRow.background = background1
         titleRow.minimumSize = Dimension(width, 80)
         titleRow.layout = FlowLayout(FlowLayout.CENTER, 0, 0)
         titleRow.add(titleLabel)
 
-        outputAreaPane.background = background3
-        outputAreaPane.minimumSize = Dimension(width - 30, 190)
-        outputAreaPane.size = Dimension(width - 30, 190)
-        outputAreaPane.layout = FlowLayout(FlowLayout.CENTER, 0, 0)
-        outputAreaPane.add(outputArea)
-
-        outputArea.minimumSize = Dimension(outputAreaPane.width, outputAreaPane.height)
-        outputArea.size = Dimension(outputAreaPane.width, outputAreaPane.height)
-
-        // q: why is the output area pane only as big as the output area text?
-        // a: because the output area text is the only thing in the output area pane
-        outputText.minimumSize = Dimension(outputAreaPane.width, outputAreaPane.height)
-        outputText.size = Dimension(outputAreaPane.width, outputAreaPane.height)
+        // the scrollbar
+        outputArea.minimumSize = Dimension(outputRow.width, outputRow.height)
+        outputArea.size = Dimension(outputRow.width, outputRow.height)
 
         outputRow.background = background1
         outputRow.minimumSize = Dimension(width, 190)
-        outputRow.add(outputAreaPane)
+        outputRow.layout = GridLayout(1, 1)
+        outputRow.add(outputArea)
 
         actionRow.background = background1
         actionRow.minimumSize = Dimension(width, 80)
-        actionRow.layout = FlowLayout(FlowLayout.CENTER, 0, 0)
+        actionRow.layout = FlowLayout(FlowLayout.CENTER, 10, 0)
+        // this make sthe buttons move down
+        actionRow.border = BorderFactory.createEmptyBorder(height + 60, 0, 10, 0)
         if (support != null) {
             actionRow.add(supportButton)
             actionRow.add(space)
@@ -149,13 +145,13 @@ class Menu : JFrame(
     }
 
     private fun updateOutputText() {
-        outputText.text = "<html><ul>${outputList.joinToString("") { "<li>$it</li>" }}</ul></html>"
+        //append to outputText
+        outputText.text = outputList.joinToString("<html><ul>${outputList.joinToString("") { "<li>$it</li>" }}</ul><br></html>")
     }
 
     fun handleMoved(name: String) {
         // add a line of orange text (#F17605) to the output area that says
         // "Moved $name"
-
         outputList.add("<font color=\"#F17605\">Moved $name</font>")
         updateOutputText()
     }
